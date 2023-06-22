350a19f699f1784a53dd93332c6cd22c3d4dc4d6
K-Saaan <111870436+K-Saaan@users.noreply.github.com>
2023-06-22 17:42:58 +0900

    feat. 06/22 project commit

---
+++ src/main/java/com/example/brokurly_batch/test/SimpleBatch.java
@@ -0,0 +1,45 @@
+package com.example.brokurly_batch.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleBatch {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+
+    @Bean(name = "simpleJob")
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters['param']}") String param) {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step1");
+                    log.info(">>>>> param : " + param);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
---
+++ src/test/java/com/example/brokurly_batch/BrokurlyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brokurly_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokurlyBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
